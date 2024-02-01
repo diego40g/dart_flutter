2002c1c19777d92971e925ec2dcdacc7974ba34a
diego40g <diegopaznaula@hotmail.com>
2024-02-01 09:12:40 -0500

    Adding primitive data to understand the differents atributes

---
+++ 02-primitive-data.dart
@@ -0,0 +1,27 @@
+void main() {
+    final String pokemon = 'Ditto';
+    final int helpPoint = 10;
+    final bool isAlive = true;
+    final List<String>abilities = <String>['Transform', 'Impersonate', 'Morph'];
+    //final abilities = ['Transform', 'Impersonate', 'Morph']
+    //final abilities = <String>['Transform', 'Impersonate', 'Morph']
+    final List<String>sprites = ['ditto.png', 'ditto.gif', 'ditto.jpg'];
+
+    // No es necesario el ? para dynamic por que la tiene ese atributo
+    dynamic errorMessage = "Ditto doesn't have any abilities";
+    errorMessage = true;
+    errorMessage = 10;
+    errorMessage = [1,2,3,4,5];
+    errorMessage = {1,2,3,4,5};
+    errorMessage = () => {return true;};
+    errorMessage = null;
+    //errorMessage +=1;//Debe ser error porque no se suma 1 a un null
+
+    print("""
+        $pokemon
+        $helpPoint
+        $isAlive
+        $abilities
+        $sprites
+    """);
+}
