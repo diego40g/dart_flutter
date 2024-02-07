e45973d1cff952e04ffd67ea487b63b047ea53cf
diego40g <diegopaznaula@hotmail.com>
2024-02-07 00:13:15 -0500

    Add PlantType enum and update EnergyPlant class

--- 01-dart-intro/12-abstract-enum.dart
+++ 01-dart-intro/12-abstract-enum.dart
@@ -1,7 +1,17 @@
 void main() {
-  final windPlant = EnergyPlant();
+  //final windPlant = EnergyPlant();
 }
+
+enum PlantType { wind, solar, hydro }
 
 abstract class EnergyPlant {
   double energyLeft;
+  PlantType type; // 1: Wind, 2: Solar, 3: Hydro
+
+  EnergyPlant({
+    required this.energyLeft,
+    required this.type,
+  });
+
+  void consumeEnergy(double energy);
 }
