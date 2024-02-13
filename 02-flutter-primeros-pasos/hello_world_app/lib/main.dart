fd01554012b4326b8ad141524c7914c7da314e35
diego40g <diegopaznaula@hotmail.com>
2024-02-13 01:32:36 -0500

    adding a new widget of flutter

--- 02-flutter-primeros-pasos/hello_world_app/lib/main.dart
+++ 02-flutter-primeros-pasos/hello_world_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp( // Add const because the widget is immutable not dinamic
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: Text('Hello World')),
-      )
+      home: CounterScreen()
     );
   }
 }
