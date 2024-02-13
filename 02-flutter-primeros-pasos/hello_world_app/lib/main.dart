49f2f3415596166f94b22a1de2f564a0af0b30ae
diego40g <diegopaznaula@hotmail.com>
2024-02-13 01:25:30 -0500

    deleting stick develop and some comments to understan code

--- 02-flutter-primeros-pasos/hello_world_app/lib/main.dart
+++ 02-flutter-primeros-pasos/hello_world_app/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key}); // Add super.key
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp( // Add const because the widget is immutable not dinamic
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(child: Text('Hello World')),
       )
