a724b9809d5d9ab2b65fe85baaefbf7bba649676
diego40g <diegopaznaula@hotmail.com>
2024-03-19 23:28:01 -0500

    Refactor HerMessageBubble widget to display dynamic message text

--- 03-yes-no-maybe/yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
+++ 03-yes-no-maybe/yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
 
 class HerMessageBubble extends StatelessWidget {
-  const HerMessageBubble ({super.key});
+  final Message message;
+  const HerMessageBubble({super.key, required this.message});
 
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
-    
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
-            color: colors.secondary,
-            borderRadius: BorderRadius.circular(20)
-          ),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text('Hola mundo!!!!', style: TextStyle(color: Colors.white),),
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              message.text,
+              style: const TextStyle(color: Colors.white),
+            ),
           ),
         ),
-        const SizedBox(height: 5,),
-
+        const SizedBox(
+          height: 5,
+        ),
         _ImageBubble(),
-
-        const SizedBox(height: 10,),
+        const SizedBox(
+          height: 10,
+        ),
       ],
     );
   }
@@ -33,14 +38,13 @@ class HerMessageBubble extends StatelessWidget {
 class _ImageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Image.network(
-        'https://yesno.wtf/assets/no/16-b66d2db7543f5259c86abc166d6901cf.gif', 
-        width: size.width*0.70,
+        'https://yesno.wtf/assets/no/16-b66d2db7543f5259c86abc166d6901cf.gif',
+        width: size.width * 0.70,
         height: 150,
         fit: BoxFit.cover,
         loadingBuilder: (context, child, loadingProgress) {
@@ -54,8 +58,8 @@ class _ImageBubble extends StatelessWidget {
           )*/
           // Circle to await the image
           return const CircularProgressIndicator(
-              strokeWidth: 2,
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+            strokeWidth: 2,
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
           );
         },
       ),
