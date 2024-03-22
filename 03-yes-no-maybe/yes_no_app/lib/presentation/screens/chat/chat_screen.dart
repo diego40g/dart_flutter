2825611e86cc9a16cacbfc0dc285cacac5e83396
diego40g <diegopaznaula@hotmail.com>
2024-03-21 21:28:46 -0500

    Add chat scroll controller to ListView.builder

--- 03-yes-no-maybe/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ 03-yes-no-maybe/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -40,6 +40,7 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
                 child: ListView.builder(
+              controller: chatProvider.chatScrollController,
               itemCount:
                   chatProvider.messageList.length, // Cantidad de elementos
               itemBuilder: (context, index) {
